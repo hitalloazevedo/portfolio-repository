176a631fa42ac5e242661074e0ed026b820b4c6e
hitalloazevedo <azevedohitallo@gmail.com>
2024-12-24 12:21:39 -0300

    atualizando a interface do repositorio, possibilitando o retorno de undefined

--- src/repositories/IProjectsRepository.ts
+++ src/repositories/IProjectsRepository.ts
@@ -1,7 +1,7 @@
 import { Project } from "../entities/Project";
 
 export interface IProjectsRepository {
-    findAll(): Promise<Project[]>
-    findByTitle(title: string): Promise<Project> | null;
+    findAll(): Promise<Project[] | undefined>
+    findByTitle(title: string): Promise<Project | undefined>;
     save(project: Project): Promise<void>;
 }
