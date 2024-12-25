395c7b63d13fbf0902116e6e821c3a74366f8640
hitalloazevedo <azevedohitallo@gmail.com>
2024-12-25 17:39:10 -0300

    feat: protegendo rotas com autenticação

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -3,22 +3,23 @@ import { createProjectController } from "../useCases/projects/CreateProject";
 import { getAllProjectController } from "../useCases/projects/GetAllProject";
 import { updateProjectController } from "../useCases/projects/UpdateProject";
 import { deleteProjectController } from "../useCases/projects/DeleteProject";
+import { authMiddlewareController } from "../middlewares/AuthMiddleware";
 
 const projectRoutes = Router();
 
-projectRoutes.post('/projects', (request: Request, response: Response) => {
+projectRoutes.post('/projects', authMiddlewareController, (request: Request, response: Response) => {
     createProjectController.handle(request, response);
 });
 
 projectRoutes.get('/projects', (request: Request, response: Response) => {
-    getAllProjectController.handle(request, response);
+    getAllProjectController.handle(request, response)
 })
 
-projectRoutes.patch('/projects/:uuid', (request: Request, reponse: Response) => {
+projectRoutes.patch('/projects/:uuid', authMiddlewareController,(request: Request, reponse: Response) => {
     updateProjectController.handle(request, reponse);
 })
 
-projectRoutes.delete('/projects/:uuid', (request: Request, response: Response) => {
+projectRoutes.delete('/projects/:uuid', authMiddlewareController, (request: Request, response: Response) => {
     deleteProjectController.handle(request, response);
 })
 
