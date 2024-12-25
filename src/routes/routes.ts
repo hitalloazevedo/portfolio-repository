5e37abdaaa05ce1d6bc59c679e05fb79fc4d3ecf
hitalloazevedo <azevedohitallo@gmail.com>
2024-12-25 20:37:14 -0300

    feat: adicionando a rota de skills

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authRoutes } from "./authRoutes";
 import { projectRoutes } from "./projectRoutes";
+import { skillRoutes } from "./skillRoutes";
 
 const router = Router();
 
 router.use(authRoutes);
 router.use(projectRoutes);
+router.use(skillRoutes);
 
 export { router };
