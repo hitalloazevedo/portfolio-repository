959aa43c1ddd31ba0c0a1f0c532c23500c3eefc3
hitalloazevedo <azevedohitallo@gmail.com>
2024-12-25 21:47:17 -0300

    config: instalando e configurando cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 import { router } from './routes/routes';
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.ALLOWED_ORIGINS?.split(' ')
+}))
+
 app.use(express.json())
 app.use(router)
+
 
 export { app };
