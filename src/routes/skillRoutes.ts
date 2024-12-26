0e24423fbe1f8f803a6e2e31d9cb03ea13b8da6f
hitalloazevedo <azevedohitallo@gmail.com>
2024-12-25 21:09:19 -0300

    feat: adicionando rota que retorna todas as skills

--- src/routes/skillRoutes.ts
+++ src/routes/skillRoutes.ts
@@ -2,8 +2,13 @@ import { Router, Response, Request } from "express";
 import { authMiddlewareController } from "../middlewares/AuthMiddleware";
 import { createSkillController } from "../useCases/skills/CreateSkill";
 import { updateSkillController } from "../useCases/skills/UpdateSkill";
+import { getAllSkillsController } from "../useCases/skills/GetAllSkills";
 
 const skillRoutes = Router();
+
+skillRoutes.get('/skills',  (request: Request, response: Response) => {
+    getAllSkillsController.handle(request, response);
+})
 
 skillRoutes.post('/skills', authMiddlewareController, (request: Request, response: Response) => {
     createSkillController.handle(request, response);
