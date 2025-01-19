9e1a8e13877a7bec9a46d5abdd13915dd39fe72e
hitalloazevedo <azevedohitallo@gmail.com>
2025-01-19 17:53:25 -0300

    feat: add destructuring and svg to baes64 conversion before save in the database

--- src/useCases/skills/UpdateSkill/UpdateSkillUseCase.ts
+++ src/useCases/skills/UpdateSkill/UpdateSkillUseCase.ts
@@ -1,13 +1,21 @@
 import { ISkillsRepository } from "../../../repositories/ISkillsRepository";
 import { IUpdateSkillRequestDTO } from "./UpdateSkillDTO";
+import { svgToBase64 } from "../../../utils/svgImageHandler";
 
 export class UpdateSkillUseCase {
     constructor (
         private skillsRepository: ISkillsRepository
     ) {}
 
-    async execute(uuid: string, data: Partial<IUpdateSkillRequestDTO>) {
+    async execute(uuid: string, { title, description, svg_image }: Partial<IUpdateSkillRequestDTO>) {
         const _id = await this.skillsRepository.getIdbyUuid(uuid);
-        await this.skillsRepository.update(_id, data);
+
+        let base64Svg: string | undefined;
+
+        if (svg_image) {
+            base64Svg = svgToBase64(svg_image);
+        }
+
+        await this.skillsRepository.update(_id, { title, description, svg_image: base64Svg});
     }
 }
