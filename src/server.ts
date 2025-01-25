4833889242c530c58efd97e566fe75529042a2a2
hitalloazevedo <azevedohitallo@gmail.com>
2025-01-25 12:44:54 -0300

    feat: calling stay awake cron job after initialize the server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { app } from "./app";
 import dotenv from 'dotenv'
+import { StayAwakeService } from "./services/implementations/StayAwakeService";
 
 dotenv.config();
 
 const port = Number(process.env.PORT) || 3000;
 
+const awakeService = new StayAwakeService("https://projects-repository.onrender.com/", 1);
+
 app.listen(port, () => {
-    console.log(`Server running!`)
+    console.log(`Server running!`);
+
+    // cronjob to make the backend alive
+    awakeService.start();
 })
