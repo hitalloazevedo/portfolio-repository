2a8b1d6b45f9591b5c8ffbb2f11b7de3bf1c7a92
hitalloazevedo <azevedohitallo@gmail.com>
2025-02-15 23:07:39 -0300

    feat: handling closing connection by closing application

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,16 @@ const awakeService = new StayAwakeService("https://projects-repository.onrender.
 MongoDBRepository.openConnection();
 
 app.listen(port, () => {
+    
     console.log(`Server running on port ${port}!`);
 
     // start cronjob to keep the application alive
     awakeService.start();
 })
+
+// Add shutdown handling to close the MongoDB connection when the app shuts down
+process.on('SIGINT', async () => {
+    console.log("Gracefully shutting down...");
+    await MongoDBRepository.closeConnection();
+    process.exit(0); // Exit the process
+});
