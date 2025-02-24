bbb9e73298834ac70d159562791393c3e49a6300
hitalloazevedo <azevedohitallo@gmail.com>
2025-02-23 21:56:45 -0300

    feat: create File entity

---
+++ src/entities/File.ts
@@ -0,0 +1,9 @@
+export class File {
+    public fileName: string;
+    public filePath: string;
+
+    constructor (fileName: string, filePath: string){
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+}
