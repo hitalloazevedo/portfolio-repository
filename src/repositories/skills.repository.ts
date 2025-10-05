8adfca2eb707a18d3388df60dedd97eba7342590
Hitallo Azevedo <azevedohitallo@gmail.com>
2025-10-05 07:38:05 -0300

    feat: skill repository update

--- src/repositories/skills.repository.ts
+++ src/repositories/skills.repository.ts
@@ -4,8 +4,6 @@ import { CreateSkillDTO } from "../use-cases/skill.usecase";
 export interface SkillRepository {
     findAll(): Promise<Skill[] | null>
     findByTitle(title: string): Promise<Skill | null>;
-    getIdbyUuid(uuid: string): Promise<unknown>;
     save(skill: CreateSkillDTO): Promise<void>;
-    update(_id: unknown, newData: Partial<Skill>): Promise<void>;
     delete(title: string): Promise<void>;
 }
