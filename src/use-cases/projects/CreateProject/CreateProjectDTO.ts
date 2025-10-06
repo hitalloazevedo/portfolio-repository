0a2001569c523951272b46c3180ec606d9e4822b
hitalloazevedo <azevedohitallo@gmail.com>
2025-10-06 00:17:54 -0300

    unused files

--- src/use-cases/projects/CreateProject/CreateProjectDTO.ts
+++
@@ -1,8 +0,0 @@
-export interface ICreateProjectRequestDTO {
-    title: string;
-    description: string;
-    image_url: string;
-    repo_url: string;
-    deploy_url: string;
-    tech_stack: Array<string>;
-}
--- src/use-cases/projects/UpdateProject/UpdateProjectDTO.ts
+++
@@ -1,5 +0,0 @@
-import { Project } from "../../../entities/project";
-
-export interface IUpdateProjectRequestDTO extends Project {
- 
-};
